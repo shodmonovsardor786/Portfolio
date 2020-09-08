b28ebd6f527c55532ee48adb36d586803db30dc1
Sardor <shodmovsardor786@yandex.ru>
2020-09-08 12:03:23 +0500

    My portfolio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var scroll = new SmoothScroll('a[href*="#"]');
+
+let navElement = document.querySelector('.nav')
+let navItem = document.querySelector('.nav__item')
+let projectHeadingElement = document.querySelector('.my-projects__heading')
+ 
+window.addEventListener('scroll', e => {
+	if(window.pageYOffset > 80) {	navElement.classList.add('fixed_style')		}
+	if(window.pageYOffset < 80) {	navElement.classList.remove('fixed_style')	}
+
+	if(window.pageYOffset > 660) {	navItem.classList.remove('home')	}
+	if(window.pageYOffset < 620) {	navItem.classList.add('home')		}
+
+	if(window.pageYOffset > 880) {	projectHeadingElement.classList.add('my-projects__heading--style')	}
+})
