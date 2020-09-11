491c0ed665766a31e0f18078c804c33d13e4cafb
Sardor <shodmovsardor786@yandex.ru>
2020-09-11 15:14:06 +0500

    FINISH RESPONSIVE DISPLAY

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ let projectHeadingElement = document.querySelector('.my-projects__heading')
 window.addEventListener('scroll', e => {
 	if(window.pageYOffset > 80) {	navElement.classList.add('fixed_style')		}
 	if(window.pageYOffset < 80) {	navElement.classList.remove('fixed_style')	}
+	if(window.innerWidth < 800) {	navElement.classList.remove('fixed_style')
+	}
 
 	if(window.pageYOffset > 660) {	navItem.classList.remove('home')	}
 	if(window.pageYOffset < 620) {	navItem.classList.add('home')		}
