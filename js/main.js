644f74f610b5046559f239f64e06e1b40450bfd8
Sardor <shodmovsardor786@yandex.ru>
2020-09-13 23:02:47 +0500

    Remove icons set online icons and minify images

--- js/main.js
+++ js/main.js
@@ -9,8 +9,5 @@ window.addEventListener('scroll', e => {
 	if(window.pageYOffset < 80) {	navElement.classList.remove('fixed_style')	}
 	if(window.innerWidth < 800) {	navElement.classList.remove('fixed_style')
 	}
-
-	// if(window.pageYOffset > 660) {	navItem.classList.remove('home')	}
-	// if(window.pageYOffset < 620) {	navItem.classList.add('home')		}
 })
 
