c209350e61beb3ecb1d94624604cbf4c0b38f56d
Sardor <shodmovsardor786@yandex.ru>
2020-09-13 13:40:37 +0500

    Remove project heading style

--- js/main.js
+++ js/main.js
@@ -12,7 +12,5 @@ window.addEventListener('scroll', e => {
 
 	// if(window.pageYOffset > 660) {	navItem.classList.remove('home')	}
 	// if(window.pageYOffset < 620) {	navItem.classList.add('home')		}
-
-	if(window.pageYOffset > 880) {	projectHeadingElement.classList.add('my-projects__heading--style')	}
 })
 
